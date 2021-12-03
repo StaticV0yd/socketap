0e83350a9bdd7f3a2183fe0098139e54f4ed4402
StaticV0yd <staticv0yd@protonmail.com>
2021-12-02 20:00:45 -0500

    Started writing a function to send edited packets and moved socket creation to main so the socket can be passed to other functions.

--- cmd/main.go
+++ cmd/main.go
@@ -7,39 +7,36 @@ import (
 	"github.com/sockon-script/packet"
 )
 
-func readFromSocket() {
+func readFromSocket(fd int) {
+
+	data := make([]byte, 1024)
+	for {
+		syscall.Recvfrom(fd, data, 0)
+		// fmt.Println(data)
+		// fmt.Println()
+		frame := packet.DataToFrame(data)
+		fmt.Println(frame.ToHexString())
+		// fmt.Println()
+		// fmt.Println(hex.EncodeToString(data))
+	}
+}
+
+func sendFromSocket(fd int) {
+	var addr syscall.SockaddrLinklayer
+	addr.Protocol = syscall.ETH_P_ALL
+	//addr.Ifindex = interf.Index
+}
+
+func main() {
 	fd, err := syscall.Socket(syscall.AF_PACKET, syscall.SOCK_RAW, 0x0300)
+	defer syscall.Close(fd)
 	if err != nil {
-		syscall.Close(fd)
 		panic(err)
 	}
-	defer syscall.Close(fd)
 	err = syscall.BindToDevice(fd, "wlp115s0")
 	if err != nil {
 		panic(err)
 	}
-	data := make([]byte, 1024)
-	//for {
-	syscall.Recvfrom(fd, data, 0)
-	// fmt.Println(data)
-	// fmt.Println()
-	frame := packet.DataToFrame(data)
-	fmt.Println(frame.ToHexString())
-	// fmt.Println()
-	// fmt.Println(hex.EncodeToString(data))
-	//}
-}
+	readFromSocket(fd)
 
-func main() {
-	//fmt.Println(packet.IPv4)
-	//test := packet.EthernetFrameFromString()
-	// fd, err := syscall.Socket(syscall.AF_PACKET, syscall.SOCK_RAW, syscall.ETH_P_ALL)
-
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// fmt.Println("Obtained fd ", fd)
-	// defer syscall.Close(fd)
-	readFromSocket()
 }
